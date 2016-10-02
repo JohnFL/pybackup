d5a1d725274aca647cc704dcf84c3af0ba527761
JohnLure <dengmiao66@gmail.com>
2016-10-02 11:10:32 +0800

    Update 2.7-5-while.py

--- 2.7-5-while.py
+++ 2.7-5-while.py
@@ -1,11 +1,11 @@
 num=10
 print 'Guess the number in my head.'
-bingo=False
+bingo=False#将False赋给bingo
 
-while bingo==False:
-    answer=input()
+while bingo==False:#这句话意思是判断bingo==False是否为真，判断的结果是bool为True，所以继续执行while内部的代码
+    answer=input()#将待输入量赋给answer
     
-    if answer<num:
+    if answer<num:#假如输入的值小于num的值10，则执行if内部的代码，输出显示 too small！
         print'too small!'
 
     if answer>num:
@@ -13,4 +13,4 @@ while bingo==False:
 
     if answer==num:
         print'bingo!'
-        bingo=True
+        bingo=True#在输入值和num的设定值相等的时候，需要结束这个while循环，将True赋给bingo，此时while遇到
